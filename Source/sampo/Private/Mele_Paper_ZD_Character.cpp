1d8377cdc4f033c09d9fbb2208cecf41cf0499f8
hathiso <gleb.dne@mail.ru>
2024-07-19 20:48:07 +0300

    v0.0.1(R)

--- Source/sampo/Private/Mele_Paper_ZD_Character.cpp
+++ Source/sampo/Private/Mele_Paper_ZD_Character.cpp
@@ -1,5 +1,55 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "Mele_Paper_ZD_Character.h"
+#include "EnhancedInputSubsystems.h"
+#include "EnhancedInputComponent.h"
+#include "InputAction.h"
+#include "InputMappingContext.h"
 
+//--------------------------------------------------------------------------------------------------
+//AMele_Paper_ZD_Character::AMele_Paper_ZD_Character()
+//{
+//	PrimaryActorTick.bCanEverTick = true;
+//	AutoPossessPlayer = EAutoReceiveInput::Player0;
+//}
+//--------------------------------------------------------------------------------------------------
+void AMele_Paper_ZD_Character::PostLoad()
+{
+	Super::PostLoad();
+	AutoPossessPlayer = EAutoReceiveInput::Player0;
+	GEngine->AddOnScreenDebugMessage(-1, 10, FColor::Blue, "On_Spavn_Plaer");
+}
+//--------------------------------------------------------------------------------------------------
+void AMele_Paper_ZD_Character::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+	//LocStart = GetActorLocation();
+	//LocStart.X = GetActorLocation().X + 10;
+	////LocMele.Y = 0;
+	//SetActorLocation(LocStart);
+}
+//--------------------------------------------------------------------------------------------------
+void AMele_Paper_ZD_Character::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+	APlayerController* PlayerController = Cast<APlayerController>(GetController());
+	UEnhancedInputLocalPlayerSubsystem *sub_system_input = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PlayerController->GetLocalPlayer());
+	sub_system_input->ClearAllMappings();
+	sub_system_input->AddMappingContext(Input_Mapping_Context, 0);
+	UEnhancedInputComponent *Input = Cast<UEnhancedInputComponent>(PlayerInputComponent);
+	Input->BindAction(Move_Jump_Action, ETriggerEvent::Triggered, this, &AMele_Paper_ZD_Character::Move_Jump);
+	//Input->BindAction("Move_Jump", IE_Pressed, this, &AMele_Paper_ZD_Character::Move_Jump);
+	//InputComponent->BindAxis("Move_RL", this, &AMele_Paper_ZD_Character::Move_RL);
+	GEngine->AddOnScreenDebugMessage(-1, 10, FColor::Blue, "set_input");
+}
+//--------------------------------------------------------------------------------------------------
+//void AMele_Paper_ZD_Character::Move_RL(float AxisValue)
+//{
+//	CurrentVelocity.X = FMath::Clamp(AxisValue, -1.0f, 1.0f) * 100.0f;
+//	GEngine->AddOnScreenDebugMessage(-1, 10, FColor::Blue, "Move");
+//}
+//--------------------------------------------------------------------------------------------------
+void AMele_Paper_ZD_Character::Move_Jump(const FInputActionValue& Value)
+{
+	GEngine->AddOnScreenDebugMessage(-1, 10, FColor::Blue, "Jump");
+	ACharacter::Jump();
+}
+//--------------------------------------------------------------------------------------------------
--- Source/sampo/Public/Mele_Paper_ZD_Character.h
+++ Source/sampo/Public/Mele_Paper_ZD_Character.h
@@ -1,17 +1,28 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
 
 #include "CoreMinimal.h"
 #include "PaperZDCharacter.h"
 #include "Mele_Paper_ZD_Character.generated.h"
+//#include "InputActionValue.h"
 
-/**
- * 
- */
+class UInputMappingContext;
+//--------------------------------------------------------------------------------------------------
 UCLASS()
 class SAMPO_API AMele_Paper_ZD_Character : public APaperZDCharacter
 {
 	GENERATED_BODY()
-	
+	FVector LocStart;
+public:
+	UPROPERTY(EditDefaultsOnly, Category = "EnhancedInput")
+	UInputMappingContext* Input_Mapping_Context;
+	FVector CurrentVelocity;
+	virtual void PostLoad();
+	virtual void Tick(float DeltaTime);
+	virtual void SetupPlayerInputComponent(UInputComponent* PlayerInputComponent);
+	//virtual void Move_RL(float AxisValue);
+	UPROPERTY(EditDefaultsOnly, Category = "EnhancedInput")
+	class UInputAction* Move_Jump_Action;
+	UFUNCTION()
+	virtual void Move_Jump(const FInputActionValue& Value);
+
 };
